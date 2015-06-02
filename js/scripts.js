7ef7a69249310b2b6d6f59d488cd45e20d627280
Alec Arme & Kim Dang <student@epicodus.com>
2015-06-02 11:31:17 -0700

    Build basic front end. Add form for creating new account. Display account name and balance.

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,18 @@ Account.prototype.modifyBalance = function(amount, isDeposit) {
     this.ledger.push(-amount);
   }
 }
+
+$(document).ready(function() {
+
+  $("form#new-account").submit(function(event) {
+  event.preventDefault();
+  var newAccountName = $("input#new-accountName").val();
+  var newBalance = parseInt($("input#new-balance").val());
+  var newAccount = new Account(newAccountName, newBalance);
+  $("form#new-account").fadeOut("slow");
+  $("#accountDetails").append('<h2>Account Name: </h2><br><p>' + newAccount.accountName + '</p><br><h2>Balance: </h2><br><p>' + newAccount.balance + '</p>' );
+  $("#accountDetails").delay(1000).fadeIn("slow");
+
+  });
+
+});
